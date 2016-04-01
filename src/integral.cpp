3c467a15a9a25f06b7b831020b96e09cd4200b2f
Jonathan <jonathan.spengeman@gmail.com>
2016-04-01 15:55:14 -0700

    Broken integral things that need to be fixed

--- src/integral.cpp
+++ src/integral.cpp
@@ -47,12 +47,15 @@ double Equation(){
 
 // Not working 
 string Polynomial(){
+	cout << "TEST 0" << endl;
 	string result;
 	if(!data.eof()){
 		char a = data.get();
 		if(a == '+' || a == '-'){
+			string op(1, a);
 			// Not sure what to put here
-			return "";
+			// TODO: this needs to implement the recursive returning of elements
+			return op + FactorInt();
 		} else {
 			string term = TermInt();
 			data.seekg(term.length(), data.beg);
@@ -62,8 +65,9 @@ string Polynomial(){
 	return result;
 }
 
-// Not working
+// This is fine now, I think
 string TermInt(){
+	cout << "TEST 1" << endl;
 	string result;
 	if(!data.eof()){
 		char a = data.get();
@@ -80,29 +84,35 @@ string TermInt(){
 }
 
 string FactorInt(){
+	cout << "TEST 2" << endl;
 	string monomial = Monomial();
 	char a = data.get();
 	if(a == '^'){
+		cout << "mono 0: " << data.tellg() << endl; 
 		char a_prime = data.get();
 		string num;
-		string pw(1, a);
+		string op(1, a);
 		// This only allows for numbers as exponents not expressions
 		string exponent = Digits(a_prime, num, monomial.length() + 1);
 		data.seekg(exponent.length(), data.beg);
-		monomial = monomial + pw + exponent;
+		monomial = monomial + op + exponent;
 	} else 
 		data.putback(a);
 	return monomial;
 }
 
+// TODO: Fix the infinite loop and remove print statements
 string Monomial(){
+	cout << "TEST 3" << endl;
 	if(!data.eof()){
 		char a = data.get();
 		if(isalpha(a)){
-			string num(1, a);
-			return num;
+			cout << "mono 1: " << a << endl;
+			string var(1, a);
+			return var;
 		} 
 		else if (isdigit(a)){
+			cout << "mono 2: " << a << endl;
 			string coef = Monomial2(a);
 			data.seekg(coef.length(), data.beg);
 			string var = Monomial();
